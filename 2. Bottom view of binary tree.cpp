21052f8e0d33db9bba754434a7f923e1cec328a8
kunal_cpp <kunaliesc@gmail.com>
2021-11-03 10:50:00 +0530

    Update 2. Bottom view of binary tree.cpp

--- 2. Bottom view of binary tree.cpp
+++ 2. Bottom view of binary tree.cpp
@@ -52,71 +52,30 @@ Constraints:
 1 <= Data of a node <= 10^5
 */
 
+vector <int> bottomView(Node *root){
 
-vector <int> bottomView(Node *root);
-
-// Function to Build Tree
-Node* buildTree(string str)
-{
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if(currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if(currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
+     map<int, int> mp;                          //mp[-1] = 10      mp[0] = 15
+     queue< pair<Node *,int> > q;               // pair <curr node, horizontal dis>
+     q.push({root, 0});
+   
+      while(!q.empty()){
+        auto p = q.front();
+        q.pop();
+        
+        Node *curr = p.first;
+        int hd = p.second;
+        mp[hd] = curr->data;
+        
+        if(curr->left != NULL) q.push({curr->left, hd-1});
+        if(curr->right != NULL) q.push({curr->right, hd+1});
+      }
+  
+     vector<int>vec;
+     for(auto x : mp){
+        vec.push_back(x.second);
+     }
+    
+return vec; 
 }
 
 
