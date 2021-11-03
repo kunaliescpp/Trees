b7b39f8de97fc5cffad61932d0b129017b46d756
kunal_cpp <kunaliesc@gmail.com>
2021-11-03 10:24:50 +0530

    Update 1. Top view of binary tree.cpp

--- 1. Top view of binary tree.cpp
+++ 1. Top view of binary tree.cpp
@@ -7,10 +7,10 @@ Given below is a binary tree. The task is to print the top view of binary tree. 
 visible when the tree is viewed from the top. For the given below tree
 
        1
-    /     \
-   2       3
-  /  \    /   \
-4    5  6   7
+     /   \
+    2     3
+   / \   /  \
+  4   5 6    7
 
 Top view will be: 4 2 1 3 7
 Note: Return nodes from leftmost node to rightmost node.
