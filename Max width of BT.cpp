c255e0dcfedc726a5d53ad57e8d42f86c52a4610
kunal_cpp <kunaliesc@gmail.com>
2021-11-06 15:25:37 +0530

    Update Max width of BT.cpp

--- Max width of BT.cpp
+++ Max width of BT.cpp
@@ -16,20 +16,10 @@ Output: 4
 Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
 
 Example 2:
-Input: root = [1,3,null,5,3]
-Output: 2
-Explanation: The maximum width existing in the third level with the length 2 (5,3).
-
-Example 3:
-Input: root = [1,3,2,5]
-Output: 2
-Explanation: The maximum width existing in the second level with the length 2 (3,2).
-
-Example 4:
 Input: root = [1,3,2,5,null,null,9,6,null,null,7]
 Output: 8
 Explanation: The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
- 
+
 
 Constraints:
 The number of nodes in the tree is in the range [1, 3000].
@@ -69,6 +59,5 @@ public:
     return ans;
     }
 };
-
 
 
