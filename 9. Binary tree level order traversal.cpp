0c8ff420217bb2723d7b406663bd9f39d4f68e30
kunal_cpp <kunaliesc@gmail.com>
2021-12-12 16:50:37 +0530

    Update and rename Binary tree level order traversal.cpp to 9. Binary tree level order traversal.cpp

--- 9. Binary tree level order traversal.cpp
+++ 9. Binary tree level order traversal.cpp
@@ -8,21 +8,12 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 Example 1:
 Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[9,20],[15,7]]
-
-Example 2:
-Input: root = [1]
-Output: [[1]]
-
-Example 3:
-Input: root = []
-Output: []
  
 
 Constraints:
 The number of nodes in the tree is in the range [0, 2000].
 -1000 <= Node.val <= 1000
 */
-
 
 class Solution {
 public:
@@ -54,6 +45,5 @@ public:
     return bv;
     }
 };
-
 
 
