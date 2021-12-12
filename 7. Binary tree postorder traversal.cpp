648512081d9502114f61576b81d03388f2d2adce
kunal_cpp <kunaliesc@gmail.com>
2021-12-12 16:48:56 +0530

    Update and rename Binary tree postorder traversal.cpp to 7. Binary tree postorder traversal.cpp

--- 7. Binary tree postorder traversal.cpp
+++ 7. Binary tree postorder traversal.cpp
@@ -9,23 +9,6 @@ Example 1:
 Input: root = [1,null,2,3]
 Output: [3,2,1]
 
-Example 2:
-Input: root = []
-Output: []
-
-Example 3:
-Input: root = [1]
-Output: [1]
-
-Example 4:
-Input: root = [1,2]
-Output: [2,1]
-
-Example 5:
-Input: root = [1,null,2]
-Output: [2,1]
- 
-
 Constraints:
 The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
@@ -67,7 +50,5 @@ public:
     return v;
     }
 };
-
-
 
 
