453e949e614c611fa53ceee1914f5e86e4ec0ff6
kunal_cpp <kunaliesc@gmail.com>
2021-12-12 16:49:16 +0530

    Update 7. Binary tree postorder traversal.cpp

--- 7. Binary tree postorder traversal.cpp
+++ 7. Binary tree postorder traversal.cpp
@@ -12,8 +12,6 @@ Output: [3,2,1]
 Constraints:
 The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
- 
-Follow up: Recursive solution is trivial, could you do it iteratively?
 */
 
 
