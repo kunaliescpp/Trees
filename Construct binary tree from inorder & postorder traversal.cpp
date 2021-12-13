aad6084bb2f27dd57d65add722370084d57a02d1
kunal_cpp <kunaliesc@gmail.com>
2021-12-13 13:03:10 +0530

    Update Construct binary tree from inorder & postorder traversal.cpp

--- Construct binary tree from inorder & postorder traversal.cpp
+++ Construct binary tree from inorder & postorder traversal.cpp
@@ -25,47 +25,31 @@ inorder is guaranteed to be the inorder traversal of the tree.
 postorder is guaranteed to be the postorder traversal of the tree.
 */
 
+Node* solver(int inorder[], int postorder[], int& pi, int si, int ei){
 
-class Solution {
-public:
-// Approach: preorder vector me iterate karna he and tree ko inorder arrangement ke according arrange karna he
-    
-    TreeNode* cTree(vector<int>& inorder, vector<int>& postorder, int& postIndex, int is, int ie){//inorder st
-                                                                                                 //inorder end
-        if(is > ie) return NULL;
-        TreeNode *root = new TreeNode (postorder[postIndex--]);
-        
-        int inIndex;
-        for(int i = is; i <= ie; i++){
-            if(inorder[i] == root->val){
-                inIndex = i;
-                break;
-            }
+    if(si > ei) return NULL;
+
+    Node *root = new Node (postorder[pi]);
+    pi--;
+
+    int idx;
+    for(int i = si; i <= ei; i++){
+        if(inorder[i] == root->val){
+            idx = i; break;
         }
-    
-    // As postOrder is (l r root ) so we traverse, l <- r <- root
-       root->right = cTree(inorder, postorder, postIndex, inIndex + 1, ie);    // if(index+1 > ie) return NULL
-       root->left = cTree(inorder, postorder, postIndex, is, inIndex - 1);     // if(is > inIndex-1)return NULL
-    
-    return root;
     }
-    
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder){
-        
-        int postIndex = postorder.size()-1;
-       TreeNode* first = cTree(inorder, postorder, postIndex, 0, inorder.size()-1);
-                      //cTree(inorder vec, postorder vec, postIndex, inorder start, inorder end)
-    return first;
-    }
-};
 
-// postorder tree input:
+    root->right = solver(inorder, postorder, pi, idx + 1, ei);
+    root->left = solver(inorder, postorder, pi, si, idx-1);
+}
 
-//         7
-//        / \
-//      20   15
-//     /       \
-//    3         9
+Node* construct(int inorder[], int postorder[]){
 
+    int n = postorder.size();
+
+    Node *root = solver(inorder, postorder, n-1, 0, n-1);
+
+return root;
+}
 
 
