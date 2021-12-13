60f5f48a1b75b096f56eaab7390e60635c45a83e
kunal_cpp <kunaliesc@gmail.com>
2021-12-13 11:16:07 +0530

    Update Connect nodes at same level.cpp

--- Connect nodes at same level.cpp
+++ Connect nodes at same level.cpp
@@ -7,9 +7,9 @@ Given a binary tree, connect the nodes that are at same level. You'll be given a
 
 Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right 
 for each node.
-       10                       10 ----> NULL
+       10                       10 ----> NULL                    
       / \                       / \
-     3   5       =>            3-->5 ---> NULL
+     3   5       =>            3-->5 ---> NULL   (No need to show the last pointer i.e. ---> NULL)
     / \   \                   / \   \
    4   1   2                 4-->1-->2 ---> NULL
 
