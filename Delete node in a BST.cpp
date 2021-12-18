ca1b46e29bef78535ae2f562fbbed9df75604827
kunal_cpp <kunaliesc@gmail.com>
2021-12-18 20:43:33 +0530

    Update Delete node in a BST.cpp

--- Delete node in a BST.cpp
+++ Delete node in a BST.cpp
@@ -6,10 +6,6 @@ Delete Node in a BST
 Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the 
 root node reference (possibly updated) of the BST.
 
-Basically, the deletion can be divided into two stages:
-Search for a node to remove.
-If the node is found, delete the node.
- 
 Example 1:
 Input: root = [5,3,6,2,4,null,7], key = 3
 Output: [5,4,6,2,null,null,7]
@@ -22,19 +18,9 @@ Input: root = [5,3,6,2,4,null,7], key = 0
 Output: [5,3,6,2,4,null,7]
 Explanation: The tree does not contain a node with value = 0.
 
-Example 3:
-Input: root = [], key = 0
-Output: []
- 
 
 Constraints:
-The number of nodes in the tree is in the range [0, 10^4].
--10^5 <= Node.val <= 10^5
-Each node has a unique value.
-root is a valid binary search tree.
 -10^5 <= key <= 10^5
-
-Follow up: Could you solve it with time complexity O(height of tree)?
  */
 
 
@@ -82,7 +68,5 @@ public:
     return root;   
     }
 };
-
-
 
 
