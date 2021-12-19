10d7a73ae6f6de012c72d3bb67d79ca8e64b0102
kunal_cpp <kunaliesc@gmail.com>
2021-12-19 11:16:16 +0530

    Update Diametre of binary tree.cpp

--- Diametre of binary tree.cpp
+++ Diametre of binary tree.cpp
@@ -4,8 +4,6 @@ Problem Link: https://practice.geeksforgeeks.org/problems/diameter-of-binary-tre
 Diameter of a Binary Tree 
 
 The diameter of a tree (sometimes called the width) is the number of nodes on the longest path between two end nodes. 
-The diagram below shows two trees each with diameter nine, the leaves that form the ends of the longest path are shaded
-(note that there is more than one path in each tree of length nine, but no path longer than nine nodes). 
 
 Example 1:
 Input:
